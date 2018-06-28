357a49e0461736732d54631e80e18575346ace4f
lb18 <40322896+lb18@users.noreply.github.com>
2018-06-28 17:53:35 +0530

    Update testHris.java

--- test/testHris.java
+++ test/testHris.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import pages.LoginPage;
+import pages.Logout;
 import pages.SelectBrowser;
 import pages.TimeSheet;
 
@@ -17,6 +18,7 @@ public class testHris
 	WebDriver driver;
 	LoginPage check_credentials_passed_for_login;
 	TimeSheet get_total_working_hours;
+	Logout logout;
 	
 	@BeforeClass
 	public void init()
@@ -25,16 +27,13 @@ public class testHris
 		driver = select_browser_type.launchBrowser("chrome", "https://hris.qainfotech.com/login.php");
 		check_credentials_passed_for_login = PageFactory.initElements(driver, LoginPage.class);
 		get_total_working_hours = PageFactory.initElements(driver, TimeSheet.class);
+		logout = PageFactory.initElements(driver, Logout.class);
 	}
 	
 	@Test
 	public void username_Field_Is_Empty_Should_Load_The_Same_Page_Again()
 	{
 		check_credentials_passed_for_login.fill_Fields("", "abc");
-		//WebElement username_txt_field = driver.findElement(By.id("txtUserName"));
-		//System.out.println(username_txt_field.getCssValue("border"));//getAttribute("style")));
-		//WebElement username_txt_field = driver.findElement(By.xpath("//*input(@id = 'txtUserName' AND @type = 'text)"));
-		//assertEquals(username_txt_field.getAttribute("name"), true);
 		assertEquals(driver.getCurrentUrl(),"https://hris.qainfotech.com/login.php");
 	}
 	
@@ -43,6 +42,34 @@ public class testHris
 	{
 		check_credentials_passed_for_login.fill_Fields("lokeshbisht", "Lokesh@321#");
 		assertEquals(driver.getCurrentUrl(),"https://hris.qainfotech.com:8086/time/timesheet");
+	}
+	
+	@Test(dependsOnMethods = {"logging_In_With_Valid_Credentials_Should_Load_Home_Page"})
+	public void Total_Time_Shown_In_HRIS_Timesheet_Should_Be_Correct()
+	{
 		get_total_working_hours.get_Data_From_TimeSheet();
+		WebElement total_working_hrs = driver.findElement(By.xpath("//tr[3]/td[8]/employee-timesheet/div/div/span"));
+		String tot_time = total_working_hrs.getText();
+		Assert.assertTrue(get_total_working_hours.get_Data_From_TimeSheet().equals(tot_time));
+	}
+	
+	@Test(dependsOnMethods = {"Total_Time_Shown_In_HRIS_Timesheet_Should_Be_Correct"})
+	public void after_Clicking_On_Logout_Button_User_Should_Redirect_To_Login_Page()
+	{
+		logout.logout_User();
+		WebElement logout_check = driver.findElement(By.xpath("//footer/div/div[2]/p"));
+		Assert.assertTrue(logout_check.getText().contains("QAITHRIS Copyright QA InfoTech Pvt. Ltd. 2018 rights reserved."));
+	}
+	
+	@Test(dependsOnMethods = {"after_Clicking_On_Logout_Button_User_Should_Redirect_To_Login_Page"})
+	public void after_Clicking_On_Logout_Button_User_Should_Be_Show_Copyright_Text()
+	{
+		assertEquals(driver.getCurrentUrl(),"https://hris.qainfotech.com/login.php");
+	}
+	
+	@AfterClass
+	public void closeBrowser()
+	{
+		driver.quit();
 	}
 }
